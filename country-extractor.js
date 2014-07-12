fd21a5f932fa28315c925b0f7e8cde6897870701
David Björklund <david.bjorklund@gmail.com>
2014-07-12 16:30:41 +0200

    Initial commit

---
+++ country-extractor.js
@@ -0,0 +1,12 @@
+var countryCount = require('country-count')
+  , nameExtractor = require('name-extractor')
+
+  , extract = function (input) {
+      var names = nameExtractor(input).map(function (name) {
+            return name.replace(/^[^A-Za-z\.]|,$/, '')
+          })
+
+      return Object.keys(countryCount(names))
+    }
+
+module.exports = extract
---
+++ example.js
@@ -0,0 +1,3 @@
+var extract = require('./country-extractor')
+
+console.log(extract('I used to live in Sweden, but now I live in the United States of America'))
